d5d6444f504a477572e2d2c45f3d0556b5a4d8e8
Yuki Kimoto <kimoto.yuki@gmail.com>
2020-01-25 14:32:29 +0900

    リダイレクトりするスクリプトを追加

---
+++ utils/redirect.pl
@@ -0,0 +1,26 @@
+use strict;
+use warnings;
+
+# リダイレクトのJavaScriptを追加する
+
+my ($redirect_base_url, @blog_files) = @ARGV;
+
+for my $blog_file (@blog_files) {
+  my $blog_path = "templates/blog/$blog_file";
+  
+  open my $blog_fh, '<', $blog_path
+    or die "Can't open $blog_path for read: $!";
+  
+  my $blog_content = do { local $/; <$blog_fh> };
+  
+  $blog_fh = undef;
+  
+  my $redirect_js = qq|<script>location.href='$redirect_base_url/blog/$blog_file';</script>|;
+  
+  $blog_content = "$redirect_js\n$blog_content";
+  
+  open my $blog_fh, '>', $blog_path
+    or die "Can't open $blog_path for write: $!";
+  
+  print $blog_fh $blog_content;
+}
